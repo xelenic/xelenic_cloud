a081dee8a928b8ee68c894809f1f1b49f2e09401
sanjayaharshana <sanjayaharasana@gmail.com>
2021-10-09 18:53:58 +0530

    Index Blade PHP

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -202,20 +202,10 @@
     </section>
     <!-- End brand Area -->
     <!-- Start about Area -->
-    <section class="about-area" id="about">
-        <div class="container-fluid">
-            <div class="row d-flex justify-content-end align-items-center">
-                <div class="col-lg-6 col-md-12 about-left">
-                    <h1>We Believe that Interior beautifies the</h1>
-                    <p>Inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that, as women, our behavior on the job is beyond reproach. inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that.</p>
-                    <a href="#link" class="primary-btn header-btn text-uppercase">See Details</a>
-                </div>
-                <div class="col-lg-6 col-md-12 about-right no-padding">
-                    <img class="img-fluid" src="FrontPage/img/c1.jpg" alt="">
-                </div>
-            </div>
-        </div>
-    </section>
+
+    @include('frontend.home_page_partial.home_page_map')
+
+
     <!-- End about Area -->
 
     <!-- Start service Area -->
